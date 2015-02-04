d85ed52ad4ac2c00e15fa7dc2895c10120b1fe05
SeanPWLynch <sean.lynch@smurfitkappa.ie>
2015-02-04 17:47:09 +0000

    start server build

---
+++ HorusServer/StartServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorusServer
+{
+    class StartServer
+    {
+        static void Main(string[] args)
+        {
+            HorusServer Server = new HorusServer();
+
+            Server.StartAdmin();
+
+            Server.StartClient();
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
