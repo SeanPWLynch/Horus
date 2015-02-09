433725376872baf951f35c39633ab4bde43357a2
SeanPWLynch <Sean.P.W.Lynch@gmail.com>
2015-02-09 00:47:38 +0000

    missing files

---
+++ HorusShared/HorusShared.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorusShared
+{
+    public class HorusShared
+    {
+        public static ApplicationServer.HorusServer hs = new ApplicationServer.HorusServer();
+
+        public static List<string> GetClients()
+        {
+            return hs.GetConnectedClients();
+        }
+
+        public static string GetHostName(string targetHost)
+        {
+            return hs.GetHostName(targetHost);
+        }
+
+
+    }
+}
